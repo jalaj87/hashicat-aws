b1b2eca28a3f8e48376cfac3d872fde558067fd0
Jalaj Kumar <jalaj.kumar1987@gmail.com>
2022-04-15 19:50:27 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,11 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket48574080349094"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
